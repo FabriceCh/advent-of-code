faeec59b65ed7810f5ebaf4c09c4aba50c9fc634
fabricech <fabrice.charbonneau@hotmail.com>
2023-07-27 16:53:37 -0400

    added first part of day  3

---
+++ 2022/3.py
@@ -0,0 +1,30 @@
+from utils import read_file
+
+ar = read_file("/home/fabrice/advent-of-code/2022/input")
+ar = [
+    'vJrwpWtwJgWrhcsFMMfFFhFp',
+    'jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL',
+    'PmmdzqPrVvPwwTWBwg',
+    'wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn',
+    'ttgJtRGJQctTZtZT',
+    'CrZsJsPPZsGzwwsLwLmpwMDw',
+]
+
+def letter_to_priority(letter):
+    if letter.islower():
+        return ord(letter) - 96
+    else:
+        return ord(letter) - 64  + 26
+
+def part1(ar):
+    priorities_sum = 0
+    for line in ar:
+        half_1 = line[0:len(line)//2]
+        half_2 = line[len(line)//2:]
+        for l in half_2:
+            if l in half_1:
+                priorities_sum += letter_to_priority(l)
+                break
+    print(priorities_sum)
+
+part1(ar)
