ac30c44d30ce5a43baf1203e1df007fc9e3078ff
fabricech <fabrice.charbonneau@hotmail.com>
2025-12-15 15:44:09 -0500

    2025 completed!

--- advent/2025/day09.py
+++ advent/2025/day09.py
@@ -1,10 +1,12 @@
 from functools import cache
 from types import new_class
 from advent.utils.utils import printer, read_file
+from PIL import Image
+import numpy as np
 
 ar = read_file("input")
 
-TESTING = True
+TESTING = False
 if TESTING:
     ar = read_file("input_test")
 
@@ -17,7 +19,7 @@ def process_input():
     return items
 
 def area(t1, t2):
-    return abs(t1[1] - t2[1] + 1) * abs(t1[0] - t2[0] + 1)
+    return (abs(t1[1] - t2[1]) + 1) * (abs(t1[0] - t2[0]) + 1)
 
 # @printer
 def part1():
@@ -58,20 +60,31 @@ def transform_tile(tile, x_map, y_map):
     return [x_map[tile[0]], y_map[tile[1]]]
 
 def is_intersecting(t1, t2, case):
+    # ON the line check first
     if t1[0] == t2[0]:
-        if t1[0] == case[0] and max(t1[1], t2[1]) <= case[1]:
-            return 1
-        return 0
+        if min(t1[1], t2[1]) <= case[1] and case[1] >= max(t1[1], t2[1]) and case[0] == t1[0]:
+            return 2 # ON the line
+        else:
+            return 0
+    else:
+        if min(t1[0], t2[0]) <= case[0] and case[0] >= max(t1[0], t2[0]) and case[1] == t1[1]:
+            return 2 # ON the line
+
     if t1[1] < case[1]:
-        if min(t1[0], t2[0]) < case[0] and max(t1[0], t2[0]) > case[0]:
+        if min(t1[0], t2[0]) <= case[0] and max(t1[0], t2[0]) >= case[0]:
             return 1
     return 0
 
 def is_inside(x, y, tiles):
+    # print("is", x, y, "inside")
+    if [x, y] in tiles:
+        return True
     n_intersects = 0
     for i in range(len(tiles)):
         j = (i + 1) % len(tiles)
         new_intersects = is_intersecting(tiles[i], tiles[j], [x, y]) 
+        if new_intersects == 2: # ON a line
+            return True
         n_intersects += new_intersects
     if n_intersects % 2 == 1:
         return True
@@ -81,79 +94,143 @@ def is_crossing(a, b, t1, t2):
     if a[0] == b[0] and t1[0] == t2[0] or a[1] == b[1] and t1[1] == t2[1]:
         return False
 
+    if t1 in [a, b] or t2 in [a,b]:
+        return False
+
     if t1[0] != t2[0]:
         max_x, max_y = max(t1[0], t2[0]), max(a[1], b[1])
         min_x, min_y = min(t1[0], t2[0]), min(a[1], b[1])
-        return a[0] >= min_x and a[0] <= max_x and t1[1] >= min_y and t1[1] <= max_y
+        return a[0] > min_x and a[0] < max_x and t1[1] > min_y and t1[1] < max_y
     else:
         max_x, max_y = max(a[0], b[0]), max(t1[1], t2[1])
         min_x, min_y = min(a[0], b[0]), min(t1[1], t2[1])
-        return t1[0] >= min_x and t1[0] <= max_x and a[1] >= min_y and a[1] <= max_y
+        return t1[0] > min_x and t1[0] < max_x and a[1] > min_y and a[1] < max_y
 
-def get_corners(a, b):
+def get_corners(t1, t2):
+    maxx, minx, maxy, miny = max(t1[0], t2[0]), min(t1[0], t2[0]),max(t1[1], t2[1]), min(t1[1], t2[1])
     return [
-        [min(a[0], b[0]) + 1, min(a[1], b[1]) + 1],
-        [max(a[0], b[0]) - 1, min(a[1], b[1]) + 1],
-        [min(a[0], b[0]) + 1, max(a[1], b[1]) - 1],
-        [max(a[0], b[0]) - 1, max(a[1], b[1]) - 1],
+        [maxx - 1, maxy],
+        [minx + 1, maxy],
+        [maxx - 1, miny],
+        [minx + 1, miny],
+        [maxx, miny + 1 ],
+        [minx, miny + 1 ],
+        [maxx, maxy - 1 ],
+        [minx, maxy - 1 ],
     ]
-
 
 
 def is_valid(t1, t2, tiles):
     for t in tiles:
         if t[0] > min(t1[0], t2[0]) and t[0] < max(t1[0], t2[0]) and t[1] > min(t1[1], t2[1]) and t[1] < max(t1[1], t2[1]):
-            # print("invaliid: tile inside")
+            print(f"invalid: tile {t} inside {t1} {t2}")
             return False
 
-    for corner in get_corners(t1, t2):
-        if not is_inside(corner[0], corner[1], tiles):
-            # print("invaliid: corner outside", corner)
+    for c in get_corners(t1, t2):
+        if not is_inside(c[0], c[1], tiles):
             return False
 
     for i in range(len(tiles)):
         j = (i + 1) % len(tiles)
         if is_crossing(t1, t2, tiles[j], tiles[i]):
+            return False
             # print(t1, t2, "is crossing", tiles[j], tiles[i])
-            if tiles[i][1] ==tiles[j][1]:
-                y = tiles[j][1]
-                ok = True
-                if y != max(t1[1], t2[1]):
-                    ok = ok and is_inside(t1[0], y + 1 , tiles)
-                if y != min(t1[1], t2[1]):
-                    ok = ok and is_inside(t1[0], y - 1 , tiles)
-                if not ok:
-                    # print("invaliid: crossed line is outside")
-                    return False
-            if tiles[i][0] ==tiles[j][0]:
-                x = tiles[j][1]
-                ok = True
-                if x != max(t1[0], t2[0]):
-                    ok = ok and is_inside(x + 1 , t1[1], tiles)
-                if x != min(t1[0], t2[0]):
-                    ok = ok and is_inside(x - 1 , t1[1], tiles)
-                if not ok:
-                    # print("invaliid: crossed line is outside")
-                    return False
+            # if tiles[i][1] == tiles[j][1]:
+            #     y = tiles[j][1]
+            #     if y != max(t1[1], t2[1]):
+            #         if not is_inside(t1[0], y + 1 , tiles):
+            #             return False
+            #     if y != min(t1[1], t2[1]):
+            #         if not is_inside(t1[0], y - 1 , tiles):
+            #             return False
+            # if tiles[i][0] == tiles[j][0]:
+            #     x = tiles[j][0]
+            #     if x != max(t1[0], t2[0]):
+            #         if not is_inside(x + 1 , t1[1], tiles):
+            #             return False
+            #     if x != min(t1[0], t2[0]):
+            #         if not is_inside(x - 1 , t1[1], tiles):
+            #             return False
 
     return True
 
 @printer
 def part2():
     old_tiles = process_input()
-    tiles, _, _, x_new_to_old, y_new_to_old = reduce_dims(old_tiles)
-    print(tiles)
-    mx = 0
-    for i in range(len(tiles)):
-        for j in range(i + 1, len(tiles)):
-            original_i = transform_tile(tiles[i], x_new_to_old, y_new_to_old)
-            original_j = transform_tile(tiles[j], x_new_to_old, y_new_to_old)
-            areer = area(original_i, original_j)
-            if areer > mx and is_valid(tiles[i], tiles[j], tiles):
-                mx = areer
-                print(original_i, original_j)
-    return mx
+    tiles, xon, yon, x_new_to_old, y_new_to_old = reduce_dims(old_tiles)
+    # mx = 0
+    # aaa = []
+    # for i in range(len(tiles)):
+    #     for j in range(i + 1, len(tiles)):
+    #         original_i = transform_tile(tiles[i], x_new_to_old, y_new_to_old)
+    #         original_j = transform_tile(tiles[j], x_new_to_old, y_new_to_old)
+    #         ti, tj = tiles[i], tiles[j]
+    #         # print(ti, tj)
+    #         # areer = area(ti, tj)
+    #         areer = area(original_i, original_j)
+    #         if areer > mx and is_valid(ti, tj, tiles):
+    #             mx = areer
+    #             aaa = [ti, tj]
+    #             print(original_i, original_j)
+    # # return mx
+    # # return is_valid([2,0], [0,5], tiles)
+
+    # size_x = max(list((x_new_to_old.keys())))
+    # size_y = max(list((y_new_to_old.keys())))
+    # array_data = np.zeros((size_x + 1, size_y + 1, 3), dtype=np.uint8)
+    # for i in range(len(tiles)):
+    #     t1 = tiles[i]
+    #     t2 = tiles[(i + 1) % len(tiles)]
+    #     if t1[0] != t2[0]:
+    #         max_x = max(t1[0], t2[0])
+    #         min_x = min(t1[0], t2[0])
+    #         for a in range(min_x, max_x):
+    #             array_data[a][t1[1]] = [0, 0, 255]
+    #     else:
+    #         max_y = max(t1[1], t2[1])
+    #         min_y = min(t1[1], t2[1])
+    #         for a in range(min_y, max_y):
+    #             array_data[t1[0]][a] = [0, 0, 255]
+    # t1, t2 = aaa
+    # max_x, max_y = max(t1[0], t2[0]), max(t1[1], t2[1])
+    # min_x, min_y = min(t1[0], t2[0]), min(t1[1], t2[1])
+
+    # for i in range(min_x, max_x):
+    #     array_data[i][min_y] = [255, 0, 0]
+    #     array_data[i][max_y] = [255, 0, 0]
+    # for i in range(min_y, max_y + 1):
+    #     array_data[max_x][i] = [255, 0, 0]
+    #     array_data[min_x][i] = [255, 0, 0]
+
+    # max_x, max_y = xon[94584], yon[67490] 
+    # min_x, min_y = xon[5790], yon[50147]
+    # for i in range(min_x, max_x):
+    #     array_data[i][min_y] = [0, 255, 0]
+    #     array_data[i][max_y] = [0, 255, 0]
+    # for i in range(min_y, max_y + 1):
+    #     array_data[max_x][i] = [0, 255, 0]
+    #     array_data[min_x][i] = [0, 255, 0]
+
+    # array_data[216][122] = [255, 255, 255]
+    # image = Image.fromarray(array_data, "RGB")
+    # if not TESTING:
+    #     image = image.resize(( 1000, 1000 ), Image.Resampling.LANCZOS)
+    # image.save("day9.bmp")
+    # print("image saved")
+    # return mx
+
+    a1 = [5790, 67490]
+    a2 = [94584, 50147]
+    n1 = [xon[a1[0]], yon[a1[1]]]
+    n2 = [xon[a2[0]], yon[a2[1]]]
+    # print(is_valid(n1, n2, tiles))
+    print(is_inside(31, 122, tiles))
+
+
 
 
 
 part2()
+
+
+
--- advent/2025/day12.py
+++ advent/2025/day12.py
@@ -1,3 +1,69 @@
-from advent.utils.utils import read_file
+from advent.utils.utils import printer, read_file, Grid
 
+TESTING = False
 ar = read_file("input")
+if TESTING:
+    ar = read_file("input_test")
+
+class Form:
+    def __init__(self, form, num) -> None:
+        self.form = []
+        self.num = num
+        self.size = 0
+        for l in form:
+            self.size += sum([c == '#' for c in l])
+
+class Region:
+    def __init__(self, h, w, qtes) -> None:
+        self.height = h
+        self.width = w
+        self.quantities = qtes
+
+def parse_input():
+    forms = []
+    regions = []
+    i = 0
+    while i < len(ar):
+        if ':' in ar[i] and 'x' not in ar[i]:
+            forms.append(ar[i+1: i+4])
+            i += 4
+            continue
+
+        if 'x' in ar[i]:
+            els = ar[i].split(" ")
+            dims = [int(i) for i in els[0][:-1].split("x")]
+            regions.append((dims, [int(i) for i in els[1:]]))
+        i += 1
+    return forms, regions
+
+
+
+
+@printer
+def part1():
+    total = 1000
+    rt = 0
+    forms, regions = parse_input()
+    fs: list[Form] = []
+    rs: list[Region] = []
+    for i, f in enumerate(forms):
+        fs.append(Form(f, i))
+    for r in regions:
+        rs.append(Region(r[0][0], r[0][1], r[1]))
+
+    for r in rs:
+        if r.height * r.width < sum([i * 9 for i in r.quantities]):
+            # print("no way")
+            total -= 1
+            continue
+        else:
+            print((r.height/3) * (r.width/3), sum([i  for i in r.quantities]))
+            if int((r.height/3) * (r.width/3)) >= sum([i  for i in r.quantities]):
+                rt += 1
+
+
+    return total, rt
+
+part1()
+
+
